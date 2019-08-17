c7b68cfa5f5d2618bcadb1454adcd6a492b00b44
Diego Matias <dm.diego.bh@gmail.com>
2019-08-17 19:23:42 -0300

    Added some tests

---
+++ server/src/model/search_phrases/__tests__/delete.test.ts
@@ -0,0 +1,34 @@
+import { mongo } from '../../../database';
+import { remove } from '../delete';
+import { insertOne } from '../insert_one';
+import { ISearchPhrase } from '../interfaces';
+
+const collectionName = 'search_phrases';
+
+describe('Test for search_phrases/delete.ts', () => {
+
+  beforeEach(async () => {
+    await mongo.collection(collectionName).remove();
+  });
+
+  afterAll(async () => {
+    await mongo.collection(collectionName).remove();
+    await mongo.close();
+  });
+
+  it('should delete item correctly', async () => {
+    const obj: ISearchPhrase = {
+      email: 'test@test.com',
+      howOften: 2,
+      phrase: 'drone',
+    };
+
+    const result = await insertOne(obj);
+
+    await remove(result.insertedId);
+
+    const findedObj = await mongo.collection(collectionName).getOne({_id: result.insertedId});
+
+    expect(findedObj).toEqual(null);
+  });
+});
---
+++ server/src/model/search_phrases/__tests__/update.test.ts
@@ -0,0 +1,34 @@
+import { mongo } from '../../../database';
+import { insertOne } from '../insert_one';
+import { ISearchPhrase } from '../interfaces';
+import { update } from '../update';
+
+const collectionName = 'search_phrases';
+
+describe('Test for search_phrases/update.ts', () => {
+
+  beforeEach(async () => {
+    await mongo.collection(collectionName).remove();
+  });
+
+  afterAll(async () => {
+    await mongo.collection(collectionName).remove();
+    await mongo.close();
+  });
+
+  it('should update correctly the search phrase', async () => {
+    const obj: ISearchPhrase = {
+      email: 'test@test.com',
+      howOften: 2,
+      phrase: 'drone',
+    };
+
+    const result = await insertOne(obj);
+
+    await update({_id: result.insertedId}, {email: 'test2@test.com'});
+
+    const findedObj = await mongo.collection(collectionName).getOne({_id: result.insertedId});
+
+    expect(findedObj.email).toEqual('test2@test.com');
+  });
+});
